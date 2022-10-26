2dfd966b761bd9985219fd9bd1db2374ae4f6188
Ethan Yin <yinssh1005@gmail.com>
2022-10-26 11:00:33 +0800

    Initial commit

---
+++ app/src/main/java/com/example/hellodroid/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.hellodroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String sql = "SELECT * from DUAL;";
+    }
+}
